99eb1e8effa62eba2d4b009c4a97b956a70d2a79
Levin Keller <post@levinkeller.de>
2016-03-30 19:32:41 +0200

    First working version.

---
+++ test/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var Converter = require('../index.js');
+var moment = require('moment');
+var converter = new Converter(process.env.API_KEY, process.env.API_SECRET, process.env.USERNAME, process.env.PASSWORD);
+
+describe('Simple Tests', () => {
+    it('should get all transactions', (done) => {
+        converter.extract('./reports', moment("2016-03-30").toDate(), null, (err) => {
+            if (err) {
+                return done(err);
+            }
+            done();
+        })
+    })
+});
